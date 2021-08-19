6adecd0ee19fd8236d21b7261417d845fdb4cdc1
Washington-coder <was.antonio.xy@gmail.com>
2021-08-19 13:13:21 -0400

    document A

---
+++ Chapter II/GoFinances/gofinances/src/screens/Dashboard/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+
+} from "react-native"
+
+import { Container, Title } from './styles';
+
+export function Dashboard() {
+    return (
+        <Container>
+            <Title>Data a</Title>
+        </Container>
+    );
+
+
+}
+

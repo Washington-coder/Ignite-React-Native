fffb752ac24c25aaf645fcab03ea2bdfc869dafb
Washington-coder <was.antonio.xy@gmail.com>
2021-09-02 16:05:08 -0400

    feat: Header

--- Chapter II/GoFinances/src/screens/Dashboard/index.tsx
+++ Chapter II/GoFinances/src/screens/Dashboard/index.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import {
-
+    Text,
 } from "react-native"
 
-import { Container, Title } from './styles';
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreering,
+    UserName,
+
+} from './styles';
 
 export function Dashboard() {
     return (
         <Container>
-            <Title>Dashboard aaaa</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo
+                            source={{ uri: "https://avatars.githubusercontent.com/u/82000177?v=4" }}
+                        />
+                        <User>
+                            <UserGreering>Olá,</UserGreering>
+                            <UserName>Washington</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+            </Header>
         </Container>
     );
 }

58c703273b9c47060ce98206bae540f21bd02bdf
Washington-coder <was.antonio.xy@gmail.com>
2021-09-13 14:32:48 -0400

    new: tipando cartões superiores

--- Chapter II/GoFinances/src/components/HighlightCard/index.tsx
+++ Chapter II/GoFinances/src/components/HighlightCard/index.tsx
@@ -10,18 +10,46 @@ import {
 
 } from './styles';
 
+interface Props {
+    type: "up" | "down" | "total";
+    title: string;
+    amount: string;
+    lastTransaction: string;
+}
 
-export function HighlightCard() {
+const icon = {
+    up: "arrow-up-circle",
+    down: "arrow-down-circle",
+    total: "dollar-sign",
+}
+
+
+export function HighlightCard({
+    type,
+    title,
+    amount,
+    lastTransaction,
+}: Props) {
+
     return (
-        <Container>
+        <Container type={type}>
             <Header>
-                <Title>Entrada</Title>
-                <Icon name="arrow-up-circle" />
+                <Title type={type}>
+                    {title}
+                </Title>
+                <Icon
+                    name={icon[type]}
+                    type={type}
+                />
             </Header>
 
             <Footer>
-                <Amount>R$ 17.000,40</Amount>
-                <LastTrasaction>Última entrada dia 13 de abril</LastTrasaction>
+                <Amount type={type} >
+                    {amount}
+                </Amount>
+                <LastTrasaction type={type} >
+                    {lastTransaction}
+                </LastTrasaction>
             </Footer>
         </Container>
     );

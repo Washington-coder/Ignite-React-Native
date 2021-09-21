b6f703b640d2236b94cd1e18a7f4b1a570c4f4ad
Washington-coder <was.antonio.xy@gmail.com>
2021-09-21 11:25:37 -0400

    done: Inputs styled e componentizado

--- Chapter II/GoFinances/src/screens/Register/styles.ts
+++ Chapter II/GoFinances/src/screens/Register/styles.ts
@@ -2,7 +2,8 @@ import styled from "styled-components/native"
 import { RFValue } from "react-native-responsive-fontsize"
 
 export const Container = styled.View`
-
+    flex: 1;
+    background-color: ${({ theme }) => theme.colors.background};
 `;
 
 export const Header = styled.View`
@@ -21,4 +22,10 @@ export const Title = styled.Text`
     font-size: ${RFValue(18)}px;
     color: ${({ theme }) => theme.colors.shape};
 
+`;
+
+export const Form = styled.View`
+    flex: 1;
+    width: 100%;
+    padding: 24px;
 `;

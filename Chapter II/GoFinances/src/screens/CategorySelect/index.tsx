f798c2e80b34c1cc396159d81604c96f47642861
Washington-coder <was.antonio.xy@gmail.com>
2021-09-28 11:06:57 -0400

    feat: FlatList e Button styled

--- Chapter II/GoFinances/src/screens/CategorySelect/index.tsx
+++ Chapter II/GoFinances/src/screens/CategorySelect/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
+import { FlatList } from 'react-native-gesture-handler';
+
+import { categories } from '../../utils/categories';
+
+import { Button } from "../../components/Form/Button";
+
 import {
     Container,
     Header,
     Title,
+    Category,
+    Icon,
+    Name,
+    Separator,
+    Footer,
 } from "./styles"
+
 
 interface Category {
     key: string;
@@ -27,6 +39,23 @@ export function CategorySelect({
             <Header>
                 <Title>Categoria</Title>
             </Header>
+
+            <FlatList
+                data={categories}
+                style={{ flex: 1, width: '100%' }}
+                keyExtractor={(item) => item.key}
+                renderItem={({ item }) => (
+                    <Category>
+                        <Icon name={item.icon} />
+                        <Name>{item.name}</Name>
+                    </Category>
+                )}
+                ItemSeparatorComponent={() => <Separator />}
+            />
+
+            <Footer>
+                <Button title="Selecionar" />
+            </Footer>
         </Container>
     );
 }
--- Chapter II/GoFinances/src/screens/CategorySelect/styles.ts
+++ Chapter II/GoFinances/src/screens/CategorySelect/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components/native";
+import { Feather } from "@expo/vector-icons"
 import { RFValue } from "react-native-responsive-fontsize";
 
 export const Container = styled.View`
@@ -22,5 +23,34 @@ export const Title = styled.Text`
     font-family: ${({ theme }) => theme.fonts.regular};
     color: ${({ theme }) => theme.colors.shape};
     font-size: ${RFValue(18)}px;
+`;
+
+export const Category = styled.View`
+    width: 100%;
+    padding: ${RFValue(15)}px;
+
+    flex-direction: row;
+    align-items: center;
+`;
+
+export const Icon = styled(Feather)`
+    font-size: ${RFValue(20)}px;
+    margin-right: 16px;
+`;
+
+export const Name = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.regular};
+    font-size: ${RFValue(14)}px;
+`;
+
+export const Separator = styled.Text`
+    height: 1px;
+    width: 100%;
+    background-color: ${({ theme }) => theme.colors.text};
+`;
+
+export const Footer = styled.View`
+    width: 100%;
+    padding: 24px;
 `;
 

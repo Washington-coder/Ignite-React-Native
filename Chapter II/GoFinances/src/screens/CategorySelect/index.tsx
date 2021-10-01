9401de65c0a6aa7b4b747ba44a771629f854b1c5
Washington-coder <was.antonio.xy@gmail.com>
2021-09-30 23:33:34 -0400

    new: Modal styled e estados

--- Chapter II/GoFinances/src/screens/CategorySelect/index.tsx
+++ Chapter II/GoFinances/src/screens/CategorySelect/index.tsx
@@ -24,7 +24,7 @@ interface Category {
 }
 
 interface Props {
-    category: string;
+    category: Category;
     setCategory: (category: Category) => void;
     closeSelectCategory: () => void;
 }
@@ -34,6 +34,11 @@ export function CategorySelect({
     setCategory,
     closeSelectCategory
 }: Props) {
+
+    function handleCategorySelect(category: Category) {
+        setCategory(category);
+    }
+
     return (
         <Container>
             <Header>
@@ -45,7 +50,10 @@ export function CategorySelect({
                 style={{ flex: 1, width: '100%' }}
                 keyExtractor={(item) => item.key}
                 renderItem={({ item }) => (
-                    <Category>
+                    <Category
+                        onPress={() => handleCategorySelect(item)}
+                        isActive={category.key === item.key}
+                    >
                         <Icon name={item.icon} />
                         <Name>{item.name}</Name>
                     </Category>
@@ -54,7 +62,10 @@ export function CategorySelect({
             />
 
             <Footer>
-                <Button title="Selecionar" />
+                <Button
+                    title="Selecionar"
+                    onPress={closeSelectCategory}
+                />
             </Footer>
         </Container>
     );
